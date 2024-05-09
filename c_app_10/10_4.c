2cf37c351fdf61931608c8ca53aea11a61284f2f
yoonee123 <78791337+yoonee123@users.noreply.github.com>
2024-05-09 11:34:37 +0900

    Update 10_4.c

--- c_app_10/10_4.c
+++ c_app_10/10_4.c
@@ -1,4 +1,5 @@
-// �� ���� ���������͸� ���ڷ� �޾� ���� ū ���� �ּҸ� ���� �ϴ� �Լ��� ����� ȣ���� ���ÿ�. (��)int* maxPosition(int*, int*, int*)
+// 세 개의 정수포인터를 인자로 받아 제일 큰 수의 주소를 리턴 하는 함수를 만들고 호출해 보시오. (예)int* maxPosition(int*, int*, int*)
+// 함수의 반환 타입이 포인터일 때, 그 함수가 반환하는 것은 해당 포인터가 가리키는 메모리 주소입니다.
 #include <stdio.h>
 int* maxPosition(int* p1, int* p2, int* p3)
 {
@@ -18,8 +19,8 @@ int* maxPosition(int* p1, int* p2, int* p3)
 void main4()
 {
 	int a, b, c;
-	printf("���� 3�� �Է��ϼ��� : ");
+	printf("값을 3개 입력하세요 : ");
 	scanf_s("%d %d %d", &a, &b, &c);
 	int *p = maxPosition(&a, &b, &c);
-	printf("���� ū ���� : %d", *p);
+	printf("가장 큰 값은 : %d", *p);
 }
